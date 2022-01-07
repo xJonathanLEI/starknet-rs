801ded742524359971e21c1138bdb7cf627fc509
Milan Cermak <milan.cermak@gmail.com>
2022-01-07 16:28:50 +0100

    refactor: ContractCode test with include_str

--- starknet-core/src/types/contract_code.rs
+++ starknet-core/src/types/contract_code.rs
@@ -70,10 +70,32 @@ mod tests {
 
     #[test]
     fn test_contract_code_deser() {
-        let raw = r#"{"bytecode": ["0x40780017fff7fff", "0x1", "0x208b7fff7fff7ffe", "0x400380007ffb7ffc", "0x400380017ffb7ffd", "0x482680017ffb8000", "0x3", "0x480280027ffb8000", "0x208b7fff7fff7ffe", "0x20780017fff7ffd", "0x3", "0x208b7fff7fff7ffe", "0x480a7ffb7fff8000", "0x480a7ffc7fff8000", "0x480080007fff8000", "0x400080007ffd7fff", "0x482480017ffd8001", "0x1", "0x482480017ffd8001", "0x1", "0xa0680017fff7ffe", "0x800000000000010fffffffffffffffffffffffffffffffffffffffffffffffb"], "abi": [{"type": "struct", "name": "ExternalStruct3", "members": [{"name": "x", "offset": 0, "type": "felt"}], "size": 1}, {"type": "struct", "name": "ExternalStruct2", "members": [{"name": "x", "offset": 0, "type": "(felt, ExternalStruct)"}], "size": 3}, {"type": "struct", "name": "ExternalStruct", "members": [{"name": "y", "offset": 0, "type": "(felt, felt)"}], "size": 2}, {"inputs": [], "name": "constructor", "outputs": [], "type": "constructor"}, {"inputs": [{"name": "a", "type": "felt"}, {"name": "arr_len", "type": "felt"}, {"name": "arr", "type": "felt*"}], "name": "f", "outputs": [{"name": "b", "type": "felt"}, {"name": "c", "type": "felt"}], "type": "function"}, {"inputs": [], "name": "g", "outputs": [{"name": "a", "type": "ExternalStruct3"}], "type": "function", "stateMutability": "view"}, {"inputs": [{"name": "from_address", "type": "felt"}, {"name": "a", "type": "ExternalStruct2"}], "name": "handler", "outputs": [], "type": "l1_handler"}]}"#;
-        let cc: ContractCode = serde_json::from_str(raw).unwrap();
-        assert_eq!(cc.bytecode.len(), 22);
+        let raw = include_str!("../../test-data/raw_gateway_responses/get_code/1_code.txt");
 
+        let cc: ContractCode = serde_json::from_str(raw).unwrap();
+        let abi = cc.abi.unwrap();
+
+        assert_eq!(cc.bytecode.len(), 1347);
+        if let AbiEntry::Constructor(c) = &abi[0] {
+            assert_eq!(c.name, "constructor");
+            assert_eq!(c.inputs.len(), 2);
+        } else {
+            panic!("Did not deserialize AbiEntry::Constructor properly")
+        }
+
+        if let AbiEntry::Function(f) = &abi[1] {
+            assert_eq!(f.name, "execute");
+            assert_eq!(f.inputs.len(), 5);
+        } else {
+            panic!("Did not deserialize AbiEntry::Function properly");
+        }
+        // TODO: use abi[9] to test "stateMutability" param
+
+        // fake JSON response, built from a fixture in cairo-lang repo
+        // https://github.com/starkware-libs/cairo-lang/blob/3d33c4e829a87bc3d88cf04ed6a489e788918b8b/src/starkware/starknet/compiler/starknet_preprocessor_test.py#L143
+        let raw =
+            include_str!("../../test-data/raw_gateway_responses/get_code/2_all_abi_types.txt");
+        let cc: ContractCode = serde_json::from_str(raw).unwrap();
         let abi = cc.abi.unwrap();
 
         if let AbiEntry::Struct(s) = &abi[0] {
