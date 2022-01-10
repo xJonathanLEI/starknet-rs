a0ccab2023ad818d7bc139b1a354a736d7ade69e
Jonathan LEI <me@xjonathan.dev>
2022-01-10 13:41:37 +0000

    test: add contract deployment integration test

---
+++ starknet-contract/tests/contract_deployment.rs
@@ -0,0 +1,22 @@
+use starknet_contract::{ContractArtifact, ContractFactory};
+use starknet_core::types::U256;
+
+#[tokio::test]
+async fn can_deploy_contract_to_alpha_goerli() {
+    let artifact = serde_json::from_str::<ContractArtifact>(include_str!(
+        "../test-data/oz_account_artifact.txt"
+    ))
+    .unwrap();
+    let provider = starknet_providers::SequencerGatewayProvider::starknet_alpha_goerli();
+
+    let factory = ContractFactory::new(artifact, provider).unwrap();
+
+    let result = factory
+        .deploy(vec![U256::from_dec_str("1").unwrap()], None)
+        .await;
+
+    match result {
+        Ok(_) => {}
+        Err(err) => panic!("Contract deployment failed: {}", err),
+    }
+}
