39fb66df4fc1734f8cb5bfe8f2a92260fbb26c7f
Jonathan LEI <me@xjonathan.dev>
2022-01-30 21:42:19 +0800

    feat: add `Signer` trait (#47)

---
+++ starknet-signers/src/local_wallet.rs
@@ -0,0 +1,49 @@
+use crate::{Infallible, Signer, SigningKey, VerifyingKey};
+
+use async_trait::async_trait;
+use starknet_core::{
+    crypto::{EcdsaSignError, Signature},
+    types::UnsignedFieldElement,
+};
+
+pub struct LocalWallet {
+    private_key: SigningKey,
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum SignError {
+    #[error(transparent)]
+    EcdsaSignError(EcdsaSignError),
+}
+
+impl LocalWallet {
+    pub fn from_signing_key(key: SigningKey) -> Self {
+        key.into()
+    }
+}
+
+#[async_trait]
+impl Signer for LocalWallet {
+    type GetPublicKeyError = Infallible;
+    type SignError = SignError;
+
+    async fn get_public_key(&self) -> Result<VerifyingKey, Self::GetPublicKeyError> {
+        Ok(self.private_key.verifying_key())
+    }
+
+    async fn sign_hash(&self, hash: &UnsignedFieldElement) -> Result<Signature, Self::SignError> {
+        Ok(self.private_key.sign(hash)?)
+    }
+}
+
+impl From<SigningKey> for LocalWallet {
+    fn from(value: SigningKey) -> Self {
+        Self { private_key: value }
+    }
+}
+
+impl From<EcdsaSignError> for SignError {
+    fn from(value: EcdsaSignError) -> Self {
+        Self::EcdsaSignError(value)
+    }
+}
