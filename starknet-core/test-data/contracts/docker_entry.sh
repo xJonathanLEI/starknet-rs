912af5db4c0da2d987ca6dd8e0f9f2493b49e43f
Jonathan LEI <me@xjonathan.dev>
2022-03-06 14:19:15 +0000

    fix: corrupted contract artifacts

--- starknet-core/test-data/contracts/docker_entry.sh
+++ starknet-core/test-data/contracts/docker_entry.sh
@@ -4,12 +4,12 @@ set -x
 set -e
 
 compile () {
-  starknet-compile $1 | jq -c > $2
+  starknet-compile $1  --output $2
   chown $USER_ID:$GROUP_ID $2
 }
 
-apt-get update
-apt-get install -y jq
+# Ugly hack to make contract artifacts compact
+sed -i "s/indent=4/separators=(',', ':')/g" /usr/local/lib/python3.7/site-packages/starkware/cairo/lang/compiler/cairo_compile.py
 
 # ./artifacts/oz_account.txt
 compile "/contracts/OzAccount.cairo" "/artifacts/oz_account.txt"
