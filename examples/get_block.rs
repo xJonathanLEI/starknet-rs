804d029deff72cfa7c85e06fecb517542dbdbf14
Jonathan LEI <me@xjonathan.dev>
2022-04-15 05:15:44 +0000

    doc: move examples out of README

---
+++ examples/get_block.rs
@@ -0,0 +1,11 @@
+use starknet::{
+    core::types::BlockId,
+    providers::{Provider, SequencerGatewayProvider},
+};
+
+#[tokio::main]
+async fn main() {
+    let provider = SequencerGatewayProvider::starknet_alpha_goerli();
+    let latest_block = provider.get_block(BlockId::Latest).await;
+    println!("{:#?}", latest_block);
+}
