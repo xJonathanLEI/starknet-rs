344750eee80e92773a28b3a8f8aa9da14cc32626
Jonathan LEI <me@xjonathan.dev>
2022-05-31 16:05:04 +0000

    feat: remove `Arc` wrapper in `ContractFactory`

--- examples/deploy_contract.rs
+++ examples/deploy_contract.rs
@@ -1,5 +1,3 @@
-use std::sync::Arc;
-
 use starknet::{
     contract::ContractFactory,
     core::types::{ContractArtifact, FieldElement},
@@ -11,7 +9,7 @@ async fn main() {
     let contract_artifact: ContractArtifact =
         serde_json::from_reader(std::fs::File::open("/path/to/contract/artifact.json").unwrap())
             .unwrap();
-    let provider = Arc::new(SequencerGatewayProvider::starknet_alpha_goerli());
+    let provider = SequencerGatewayProvider::starknet_alpha_goerli();
 
     let contract_factory = ContractFactory::new(contract_artifact, provider).unwrap();
     contract_factory
