67a2649d0a8744765b618e9213ef248d4181576e
Jonathan LEI <me@xjonathan.dev>
2022-06-19 14:32:24 +0800

    feat: add `felt` macro (#169)

--- tests/macros.rs
+++ tests/macros.rs
@@ -1,6 +1,6 @@
 use starknet::{
     core::utils::get_selector_from_name,
-    macros::{felt_dec, felt_hex, selector},
+    macros::{felt, felt_dec, felt_hex, selector},
 };
 use starknet_core::types::FieldElement;
 
@@ -9,6 +9,24 @@ use starknet_core::types::FieldElement;
 fn selector_can_generate_correct_selector() {
     let macro_value = selector!("balanceOf");
     let function_call_value = get_selector_from_name("balanceOf").unwrap();
+
+    assert_eq!(macro_value, function_call_value);
+}
+
+#[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test::wasm_bindgen_test)]
+fn felt_with_dec_string() {
+    let macro_value = felt!("1234567");
+    let function_call_value = FieldElement::from_dec_str("1234567").unwrap();
+
+    assert_eq!(macro_value, function_call_value);
+}
+
+#[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test::wasm_bindgen_test)]
+fn felt_with_hex_string() {
+    let macro_value = felt!("0x123456789abcdef");
+    let function_call_value = FieldElement::from_hex_be("0x123456789abcdef").unwrap();
 
     assert_eq!(macro_value, function_call_value);
 }
