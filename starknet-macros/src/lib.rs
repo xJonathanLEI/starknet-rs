61d799c48b64d0708c895e78e2781ae354bd8747
Jonathan LEI <me@xjonathan.dev>
2022-06-19 12:56:11 +0800

    feat: add macros for FieldElement literals (#168)

--- starknet-macros/src/lib.rs
+++ starknet-macros/src/lib.rs
@@ -1,5 +1,5 @@
 use proc_macro::TokenStream;
-use starknet_core::utils::get_selector_from_name;
+use starknet_core::{types::FieldElement, utils::get_selector_from_name};
 use syn::{parse_macro_input, LitStr};
 
 #[proc_macro]
@@ -14,6 +14,40 @@ pub fn selector(input: TokenStream) -> TokenStream {
     format!(
         "::starknet::core::types::FieldElement::from_mont([{}, {}, {}, {}])",
         selector_raw[0], selector_raw[1], selector_raw[2], selector_raw[3],
+    )
+    .parse()
+    .unwrap()
+}
+
+#[proc_macro]
+pub fn felt_dec(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as LitStr);
+
+    let str_value = input.value();
+
+    let felt_value = FieldElement::from_dec_str(&str_value).expect("invalid FieldElement value");
+    let felt_raw = felt_value.into_mont();
+
+    format!(
+        "::starknet::core::types::FieldElement::from_mont([{}, {}, {}, {}])",
+        felt_raw[0], felt_raw[1], felt_raw[2], felt_raw[3],
+    )
+    .parse()
+    .unwrap()
+}
+
+#[proc_macro]
+pub fn felt_hex(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as LitStr);
+
+    let str_value = input.value();
+
+    let felt_value = FieldElement::from_hex_be(&str_value).expect("invalid FieldElement value");
+    let felt_raw = felt_value.into_mont();
+
+    format!(
+        "::starknet::core::types::FieldElement::from_mont([{}, {}, {}, {}])",
+        felt_raw[0], felt_raw[1], felt_raw[2], felt_raw[3],
     )
     .parse()
     .unwrap()
