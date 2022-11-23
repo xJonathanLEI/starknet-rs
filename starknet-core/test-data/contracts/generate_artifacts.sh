eef3d00104b073c97f45386f68ac2d4da220fbad
Jonathan LEI <me@xjonathan.dev>
2022-11-23 16:59:43 +0000

    chore: bump cairo compiler to 0.10.2

--- starknet-core/test-data/contracts/generate_artifacts.sh
+++ starknet-core/test-data/contracts/generate_artifacts.sh
@@ -10,4 +10,4 @@ docker run -it --rm \
     --env "USER_ID=$(id -u)" \
     --env "GROUP_ID=$(id -g)" \
     --entrypoint "/entry.sh" \
-    shardlabs/cairo-cli:0.10.1
+    shardlabs/cairo-cli:0.10.2
