a5bd2c27dcf83962376607bb8b6fbd10d6809f1d
Thomas Marchand <thomas.marchand@tuta.io>
2023-01-09 12:15:04 +0100

    fix: formatting

--- starknet-id/src/lib.rs
+++ starknet-id/src/lib.rs
@@ -2,4 +2,6 @@ mod encoding;
 pub use encoding::{decode, encode};
 
 mod naming;
-pub use naming::{address_to_domain, domain_to_address, ResolvingError, MAINNET_CONTRACT, GOERLI_CONTRACT};
+pub use naming::{
+    address_to_domain, domain_to_address, ResolvingError, GOERLI_CONTRACT, MAINNET_CONTRACT,
+};
