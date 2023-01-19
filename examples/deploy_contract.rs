1fd40df2ceac4101d0840549705ea9ddf3bef5d5
Jonathan LEI <me@xjonathan.dev>
2023-01-19 16:44:00 +0000

    feat: contract deployment via UDC

--- examples/deploy_contract.rs
+++ examples/deploy_contract.rs
@@ -1,19 +1,40 @@
+use std::sync::Arc;
+
 use starknet::{
+    accounts::SingleOwnerAccount,
     contract::ContractFactory,
-    core::types::{ContractArtifact, FieldElement},
+    core::{
+        chain_id,
+        types::{ContractArtifact, FieldElement},
+    },
+    macros::felt,
     providers::SequencerGatewayProvider,
+    signers::{LocalWallet, SigningKey},
 };
 
 #[tokio::main]
 async fn main() {
+    // NOTE: you will need to declare this class first
     let contract_artifact: ContractArtifact =
         serde_json::from_reader(std::fs::File::open("/path/to/contract/artifact.json").unwrap())
             .unwrap();
-    let provider = SequencerGatewayProvider::starknet_alpha_goerli();
+    let class_hash = contract_artifact.class_hash().unwrap();
 
-    let contract_factory = ContractFactory::new(&contract_artifact, provider).unwrap();
+    let provider = SequencerGatewayProvider::starknet_alpha_goerli();
+    let signer = LocalWallet::from(SigningKey::from_secret_scalar(
+        FieldElement::from_hex_be("YOUR_PRIVATE_KEY_IN_HEX_HERE").unwrap(),
+    ));
+    let address = FieldElement::from_hex_be("YOUR_ACCOUNT_CONTRACT_ADDRESS_IN_HEX_HERE").unwrap();
+    let account = SingleOwnerAccount::new(provider, signer, address, chain_id::TESTNET);
+
+    // Wrapping in `Arc` is meaningless here. It's just showcasing it could be done as
+    // `Arc<Account>` implements `Account` too.
+    let account = Arc::new(account);
+
+    let contract_factory = ContractFactory::new(class_hash, account);
     contract_factory
-        .deploy(vec![FieldElement::from_dec_str("123456").unwrap()])
+        .deploy(&vec![felt!("123456")], felt!("1122"), false)
+        .send()
         .await
         .expect("Unable to deploy contract");
 }
--- starknet-contract/tests/contract_deployment.rs
+++ starknet-contract/tests/contract_deployment.rs
@@ -1,19 +1,46 @@
+use rand::{rngs::StdRng, RngCore, SeedableRng};
+use starknet_accounts::SingleOwnerAccount;
 use starknet_contract::ContractFactory;
-use starknet_core::types::{ContractArtifact, FieldElement};
+use starknet_core::{
+    chain_id,
+    types::{ContractArtifact, FieldElement},
+};
+use starknet_providers::SequencerGatewayProvider;
+use starknet_signers::{LocalWallet, SigningKey};
 
 #[tokio::test]
-#[ignore = "Legacy deploy transaction removed from live environments"]
 async fn can_deploy_contract_to_alpha_goerli() {
+    let provider = SequencerGatewayProvider::starknet_alpha_goerli();
+    let signer = LocalWallet::from(SigningKey::from_secret_scalar(
+        FieldElement::from_hex_be(
+            "00ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
+        )
+        .unwrap(),
+    ));
+    let address = FieldElement::from_hex_be(
+        "02da37a17affbd2df4ede7120dae305ec36dfe94ec96a8c3f49bbf59f4e9a9fa",
+    )
+    .unwrap();
+    let account = SingleOwnerAccount::new(provider, signer, address, chain_id::TESTNET);
+
     let artifact = serde_json::from_str::<ContractArtifact>(include_str!(
         "../test-data/artifacts/oz_account.txt"
     ))
     .unwrap();
-    let provider = starknet_providers::SequencerGatewayProvider::starknet_alpha_goerli();
 
-    let factory = ContractFactory::new(&artifact, provider).unwrap();
+    let factory = ContractFactory::new(artifact.class_hash().unwrap(), account);
+
+    let mut salt_buffer = [0u8; 32];
+    let mut rng = StdRng::from_entropy();
+    rng.fill_bytes(&mut salt_buffer[1..]);
 
     let result = factory
-        .deploy(vec![FieldElement::from_dec_str("1").unwrap()])
+        .deploy(
+            vec![FieldElement::ONE],
+            FieldElement::from_bytes_be(&salt_buffer).unwrap(),
+            true,
+        )
+        .send()
         .await;
 
     match result {
