daf923c00d25f2f6c3a3732c956b3c3af2345a08
Gregory Edison <gregory.edison1993@gmail.com>
2023-02-26 23:31:45 +0100

    fix: add inverse ec point

--- starknet-curve/src/ec_point.rs
+++ starknet-curve/src/ec_point.rs
@@ -89,6 +89,11 @@ impl ops::AddAssign<&AffinePoint> for AffinePoint {
             return;
         }
         if self.x == rhs.x {
+            if self.y == -rhs.y {
+                self.x = FieldElement::ZERO;
+                self.y = FieldElement::ZERO;
+                self.infinity = true;
+            }
             self.double_assign();
             return;
         }
