9797b54480e842505bc21f9172d42cb747522667
Jonathan LEI <me@xjonathan.dev>
2023-03-29 17:57:43 +0000

    chore: automatic compiled class hash generation

--- starknet-core/test-data/contracts/cairo1/generate_artifacts.sh
+++ starknet-core/test-data/contracts/cairo1/generate_artifacts.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 
+set -e
+
 # Deterministically generate contract artifacts
 
 docker run -it --rm \
     -v "$(pwd)/artifacts:/artifacts" \
     -v "$(pwd)/contracts:/contracts:ro" \
-    -v "$(pwd)/docker_entry.sh:/entry.sh:ro" \
+    -v "$(pwd)/docker_entry_compile.sh:/entry.sh:ro" \
     --env "USER_ID=$(id -u)" \
     --env "GROUP_ID=$(id -g)" \
     --entrypoint "/entry.sh" \
     starknet/cairo:1.0.0-alpha.6
+
+
+docker run -it --rm \
+    -v "$(pwd)/artifacts:/artifacts" \
+    -v "$(pwd)/docker_entry_compiled_hashes.sh:/entry.sh:ro" \
+    -v "$(pwd)/scripts/generate_compiled_hashes.py:/generate_hashes.py:ro" \
+    --env "USER_ID=$(id -u)" \
+    --env "GROUP_ID=$(id -g)" \
+    --entrypoint "/entry.sh" \
+    shardlabs/cairo-cli:0.11.0.1
