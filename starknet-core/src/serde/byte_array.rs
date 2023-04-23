d77f8d1a3aa1debba90fab6b26ae14171275c1de
Jonathan LEI <me@xjonathan.dev>
2023-04-24 07:47:35 +0800

    chore: bump dependencies (#360)

--- starknet-core/src/serde/byte_array.rs
+++ starknet-core/src/serde/byte_array.rs
@@ -1,4 +1,5 @@
 pub mod base64 {
+    use base64::{engine::general_purpose::STANDARD, Engine};
     use serde::{Deserialize, Deserializer, Serializer};
 
     pub fn serialize<S, T>(value: T, serializer: S) -> Result<S::Ok, S::Error>
@@ -6,7 +7,7 @@ pub mod base64 {
         S: Serializer,
         T: AsRef<Vec<u8>>,
     {
-        serializer.serialize_str(&base64::encode(value.as_ref()))
+        serializer.serialize_str(&STANDARD.encode(value.as_ref()))
     }
 
     pub fn deserialize<'de, D>(deserializer: D) -> Result<Vec<u8>, D::Error>
@@ -14,7 +15,7 @@ pub mod base64 {
         D: Deserializer<'de>,
     {
         let value = String::deserialize(deserializer)?;
-        match base64::decode(value) {
+        match STANDARD.decode(value) {
             Ok(value) => Ok(value),
             Err(err) => Err(serde::de::Error::custom(format!(
                 "invalid base64 string: {err}"
