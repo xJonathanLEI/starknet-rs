b78d3c141ae9a18c234d70f3229c123f47306f56
Daniel Bejarano <dbejarano820@gmail.com>
2023-06-21 14:04:50 -0600

    no-std support for starknet-core

--- starknet-core/src/lib.rs
+++ starknet-core/src/lib.rs
@@ -1,5 +1,10 @@
+#![cfg_attr(not(feature = "std"), no_std)]
 #![allow(clippy::comparison_chain)]
 #![doc = include_str!("../README.md")]
+
+#[cfg(all(not(feature = "std"), any(test, feature = "alloc")))]
+#[cfg_attr(test, macro_use)]
+extern crate alloc;
 
 pub mod serde;
 
