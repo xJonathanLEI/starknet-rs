d38d1ec4560472fc604e7cc3177a54b7b0127231
Jonathan LEI <me@xjonathan.dev>
2023-12-09 01:13:52 +0000

    feat: remove deprecated feeder gateway methods

--- starknet-providers/src/sequencer/models/mod.rs
+++ starknet-providers/src/sequencer/models/mod.rs
@@ -20,20 +20,17 @@ pub use transaction::{
 mod transaction_receipt;
 pub use transaction_receipt::{
     BuiltinInstanceCounter, ConfirmedReceipt as ConfirmedTransactionReceipt, Event,
-    ExecutionResources, L1ToL2Message, L2ToL1Message, Receipt as TransactionReceipt,
-    TransactionExecutionStatus, TransactionFinalityStatus, TransactionStatus,
+    ExecutionResources, L1ToL2Message, L2ToL1Message, TransactionExecutionStatus,
+    TransactionFinalityStatus, TransactionStatus,
 };
 
 mod contract_addresses;
 pub use contract_addresses::ContractAddresses;
 
-mod call_contract;
-pub use call_contract::CallContractResult;
-
 mod transaction_request;
 pub use transaction_request::{
-    AccountTransaction, AddTransactionResult, AddTransactionResultCode, CallFunction,
-    CallL1Handler, DeclareTransaction as DeclareTransactionRequest,
+    AccountTransaction, AddTransactionResult, AddTransactionResultCode,
+    DeclareTransaction as DeclareTransactionRequest,
     DeclareV1Transaction as DeclareV1TransactionRequest,
     DeclareV2Transaction as DeclareV2TransactionRequest,
     DeployAccountTransaction as DeployAccountTransactionRequest,
@@ -45,9 +42,6 @@ pub use contract::{CompressedLegacyContractClass, DeployedClass};
 
 pub mod state_update;
 pub use state_update::StateUpdate;
-
-mod fee;
-pub use fee::{FeeEstimate, FeeUnit, TransactionSimulationInfo};
 
 pub mod trace;
 pub use trace::{BlockTraces, TransactionTrace};
