79815b9b6bed5de297de73d25e316be35360b8df
Jonathan LEI <me@xjonathan.dev>
2024-03-27 18:16:18 +0000

    test: refactor pedersen tests

--- starknet-crypto/src/pedersen_hash.rs
+++ starknet-crypto/src/pedersen_hash.rs
@@ -50,37 +50,30 @@ mod tests {
     use crate::test_utils::field_element_from_be_hex;
 
     // Test case ported from:
-    //   https://github.com/starkware-libs/starkex-for-spot-trading/blob/master/src/starkware/crypto/starkware/crypto/signature/test/config/signature_test_data.json
+    //   https://github.com/starkware-libs/starkex-for-spot-trading/blob/607f0b4ce507e1d95cd018d206a2797f6ba4aab4/src/starkware/crypto/starkware/crypto/signature/test/config/signature_test_data.json
 
     #[test]
     #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test::wasm_bindgen_test)]
-    fn test_pedersen_hash_1() {
-        let in1 = field_element_from_be_hex(
-            "03d937c035c878245caf64531a5756109c53068da139362728feb561405371cb",
-        );
-        let in2 = field_element_from_be_hex(
-            "0208a0a10250e382e1e4bbe2880906c2791bf6275695e02fbbc6aeff9cd8b31a",
-        );
-        let expected_hash = field_element_from_be_hex(
-            "030e480bed5fe53fa909cc0f8c4d99b8f9f2c016be4c41e13a4848797979c662",
-        );
+    fn test_pedersen_hash() {
+        let test_data = [
+            (
+                "03d937c035c878245caf64531a5756109c53068da139362728feb561405371cb",
+                "0208a0a10250e382e1e4bbe2880906c2791bf6275695e02fbbc6aeff9cd8b31a",
+                "030e480bed5fe53fa909cc0f8c4d99b8f9f2c016be4c41e13a4848797979c662",
+            ),
+            (
+                "058f580910a6ca59b28927c08fe6c43e2e303ca384badc365795fc645d479d45",
+                "078734f65a067be9bdb39de18434d71e79f7b6466a4b66bbd979ab9e7515fe0b",
+                "068cc0b76cddd1dd4ed2301ada9b7c872b23875d5ff837b3a87993e0d9996b87",
+            ),
+        ];
 
-        assert_eq!(pedersen_hash(&in1, &in2), expected_hash);
-    }
+        for (in1, in2, expected_hash) in test_data.into_iter() {
+            let in1 = field_element_from_be_hex(in1);
+            let in2 = field_element_from_be_hex(in2);
+            let expected_hash = field_element_from_be_hex(expected_hash);
 
-    #[test]
-    #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test::wasm_bindgen_test)]
-    fn test_pedersen_hash_2() {
-        let in1 = field_element_from_be_hex(
-            "058f580910a6ca59b28927c08fe6c43e2e303ca384badc365795fc645d479d45",
-        );
-        let in2 = field_element_from_be_hex(
-            "078734f65a067be9bdb39de18434d71e79f7b6466a4b66bbd979ab9e7515fe0b",
-        );
-        let expected_hash = field_element_from_be_hex(
-            "068cc0b76cddd1dd4ed2301ada9b7c872b23875d5ff837b3a87993e0d9996b87",
-        );
-
-        assert_eq!(pedersen_hash(&in1, &in2), expected_hash);
+            assert_eq!(pedersen_hash(&in1, &in2), expected_hash);
+        }
     }
 }
