a9895554ce67ed3547a262728d18aba68451df7a
Jonathan LEI <me@xjonathan.dev>
2024-07-27 22:58:51 +0800

    docs: add docs for `starknet-curve` (#633)

--- starknet-curve/src/lib.rs
+++ starknet-curve/src/lib.rs
@@ -1,4 +1,7 @@
-#![no_std]
-#![doc = include_str!("../README.md")]
+//! A library with constant parameters for the Stark elliptic curve.
 
+#![deny(missing_docs)]
+#![no_std]
+
+/// Module containing the Stark elliptic curve parameters.
 pub mod curve_params;
