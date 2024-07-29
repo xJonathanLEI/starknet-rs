d297f8140e1fcd2862e5cdb60bdb915c8d7708d2
Jonathan LEI <me@xjonathan.dev>
2024-07-29 14:34:35 +0800

    docs: add docs for `starknet-providers` (#639)

--- starknet-providers/src/jsonrpc/transports/mod.rs
+++ starknet-providers/src/jsonrpc/transports/mod.rs
@@ -8,12 +8,16 @@ use crate::jsonrpc::{JsonRpcMethod, JsonRpcResponse};
 mod http;
 pub use http::{HttpTransport, HttpTransportError};
 
+/// Any type that is capable of producing JSON-RPC responses when given JSON-RPC requests. An
+/// implementation does not necessarily use the network, but typically does.
 #[cfg_attr(not(target_arch = "wasm32"), async_trait)]
 #[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
 #[auto_impl(&, Box, Arc)]
 pub trait JsonRpcTransport {
+    /// Possible errors processing requests.
     type Error: Error + Send + Sync;
 
+    /// Sends a JSON-RPC request to retrieve a response.
     async fn send_request<P, R>(
         &self,
         method: JsonRpcMethod,
