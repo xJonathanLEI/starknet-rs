dbe467e827bb7a02ff8e836cb08c9dd00b56f397
Jonathan LEI <me@xjonathan.dev>
2024-09-09 22:11:58 -0700

    feat: jsonrpc batch requests (#653)

--- starknet-providers/src/jsonrpc/transports/http.rs
+++ starknet-providers/src/jsonrpc/transports/http.rs
@@ -3,7 +3,10 @@ use log::trace;
 use reqwest::{Client, Url};
 use serde::{de::DeserializeOwned, Serialize};
 
-use crate::jsonrpc::{transports::JsonRpcTransport, JsonRpcMethod, JsonRpcResponse};
+use crate::{
+    jsonrpc::{transports::JsonRpcTransport, JsonRpcMethod, JsonRpcResponse},
+    ProviderRequestData,
+};
 
 /// A [`JsonRpcTransport`] implementation that uses HTTP connections.
 #[derive(Debug)]
@@ -21,6 +24,9 @@ pub enum HttpTransportError {
     Reqwest(reqwest::Error),
     /// JSON serialization/deserialization errors.
     Json(serde_json::Error),
+    /// Unexpected response ID.
+    #[error("unexpected response ID: {0}")]
+    UnexpectedResponseId(u64),
 }
 
 #[derive(Debug, Serialize)]
@@ -109,5 +115,68 @@ impl JsonRpcTransport for HttpTransport {
         let parsed_response = serde_json::from_str(&response_body).map_err(Self::Error::Json)?;
 
         Ok(parsed_response)
+    }
+
+    async fn send_requests<R>(
+        &self,
+        requests: R,
+    ) -> Result<Vec<JsonRpcResponse<serde_json::Value>>, Self::Error>
+    where
+        R: AsRef<[ProviderRequestData]> + Send + Sync,
+    {
+        let request_bodies = requests
+            .as_ref()
+            .iter()
+            .enumerate()
+            .map(|(ind, request)| JsonRpcRequest {
+                id: ind as u64,
+                jsonrpc: "2.0",
+                method: request.jsonrpc_method(),
+                params: request,
+            })
+            .collect::<Vec<_>>();
+
+        let request_count = request_bodies.len();
+
+        let request_body = serde_json::to_string(&request_bodies).map_err(Self::Error::Json)?;
+        trace!("Sending request via JSON-RPC: {}", request_body);
+
+        let mut request = self
+            .client
+            .post(self.url.clone())
+            .body(request_body)
+            .header("Content-Type", "application/json");
+        for (name, value) in &self.headers {
+            request = request.header(name, value);
+        }
+
+        let response = request.send().await.map_err(Self::Error::Reqwest)?;
+
+        let response_body = response.text().await.map_err(Self::Error::Reqwest)?;
+        trace!("Response from JSON-RPC: {}", response_body);
+
+        let parsed_response: Vec<JsonRpcResponse<serde_json::Value>> =
+            serde_json::from_str(&response_body).map_err(Self::Error::Json)?;
+
+        let mut responses: Vec<Option<JsonRpcResponse<serde_json::Value>>> = vec![];
+        responses.resize(request_bodies.len(), None);
+
+        // Re-order the responses as servers do not maintain order.
+        for response_item in parsed_response {
+            let id = match &response_item {
+                JsonRpcResponse::Success { id, .. } | JsonRpcResponse::Error { id, .. } => {
+                    *id as usize
+                }
+            };
+
+            if id >= request_count {
+                return Err(HttpTransportError::UnexpectedResponseId(id as u64));
+            }
+
+            responses[id] = Some(response_item);
+        }
+
+        let responses = responses.into_iter().flatten().collect::<Vec<_>>();
+        Ok(responses)
     }
 }
